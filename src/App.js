7a0541555c29784b179e13ca7b71cad5b8fc8567
Piyush1105 <piyushyadav1105@gmail.com>
2022-06-21 19:52:38 +0530

    deployed site

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import FirstPage from "./components/FirstPage";
 // import SecondPage from "./components/SecondPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SecondPage from "./components/SecondPage";
 function App() {
   return (
     <div className="App">
       {/* <FirstPage /> */}
-      <Router>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
           <Route path="/" element={<FirstPage />} />
           <Route path="/play" element={<SecondPage />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
